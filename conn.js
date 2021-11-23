d4618d954463548f1838ea312f81c96a3d8d46f4
imronpuji <imronpuji5@gmail.com>
2021-11-23 09:22:32 +0700

    imrn

--- conn.js
+++ conn.js
@@ -1,13 +1,13 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'trenbisn_imron',
-  password : 'tahubladak',
-  database : 'trenbisn_wa'
   // host     : 'localhost',
-  // user     : 'root',
-  // password : '',
-  // database : 'wa_services'
+  // user     : 'trenbisn_imron',
+  // password : 'tahubladak',
+  // database : 'trenbisn_wa'
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'wa_services'
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -95,14 +95,14 @@ router.post('/campaign', ({body}, res, next) => {
 
 								if((body.value - sort[sort.length - 1]['nilai']) > 0){
 									calculateDate(val.g_d_date,  async (distanceMinute, distanceDays) => {
-										if(distanceMinute > body.value && body.value == 'minutes'){
+										if(distanceMinute > body.value && body.type == 'minutes'){
 											await axios.post('http://localhost:7000/wa/send-bulk', {contact:val.nomor, message:body.messages})
 											await postCampaignDetail({kontak_id:val.kontak_id, campaign_id:resultPostCampaign.insertId}, () => {
 												
 											})	
 										}
 
-										if(distanceDays > body.value && body.value == 'days'){
+										if(distanceDays > body.value && body.type == 'days'){
 											await axios.post('http://localhost:7000/wa/send-bulk', {contact:val.nomor, message:body.messages})
 											await postCampaignDetail({kontak_id:val.kontak_id, campaign_id:resultPostCampaign.insertId}, () => {
 												
